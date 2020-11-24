e43877d44463f21454c75d06dd6245db9882d8e7
Aldo Carrion <aldo.carrion@inacapmail.cl>
2020-11-23 21:22:26 -0300

    Updates

---
+++ precache-manifest.ca546bf344bfe3fdfd3d6f9437779688.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06f659d24c9d515a16d1cf5ca97adfe",
+    "url": "/aldocarrion.github.io/OAM-proyect/index.html"
+  },
+  {
+    "revision": "b30cd83a459d89704dad",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "738127b83863abc8381c",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/css/main.1051e333.chunk.css"
+  },
+  {
+    "revision": "b30cd83a459d89704dad",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/js/2.9539222f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/js/2.9539222f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738127b83863abc8381c",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/js/main.fc7e14a8.chunk.js"
+  },
+  {
+    "revision": "584fe2b1b70b02600674",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/js/runtime-main.7051afe2.js"
+  },
+  {
+    "revision": "f24a4cd65a9adc05cdd58d8e8f6d5dfa",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/media/Logo_FULL.svg.f24a4cd6.png"
+  },
+  {
+    "revision": "4d056ba75b9e1a7770620939cba0c7cb",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/media/background-00.4d056ba7.jpg"
+  },
+  {
+    "revision": "4d056ba75b9e1a7770620939cba0c7cb",
+    "url": "/aldocarrion.github.io/OAM-proyect/static/media/background-01.4d056ba7.jpg"
+  }
+]);
